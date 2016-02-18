8616281aac579f33c3156fc15f5c4a12451ecfd0
batman1292 <batman12922@gmail.com>
2016-02-18 20:46:51 +0700

    resize and test cap

--- kinect/resize_frame.py
+++ kinect/resize_frame.py
@@ -5,6 +5,10 @@ import numpy as np
 threshold = 100
 current_depth = 650
 adj = 8
+state = 0
+bg = np.zeros([1080, 1920, 3], np.uint8)
+W = bg.shape[1]
+H = bg.shape[0]
 
 #function to get RGB image from kinect
 def get_video():
@@ -22,13 +26,16 @@ if __name__ == "__main__":
     while 1:
         #get a frame from RGB camera
         # Setup SimpleBlobDetector parameters.
-		frame = get_video()
-		frame=cv2.flip(frame,1)
-
+		#bg = np.zeros([1080, 1920, 3], np.float32)
+        		#vis = np.zeros((384, 836), np.float32)
+		#img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 		cv2.namedWindow("RGB image", cv2.WND_PROP_FULLSCREEN)
 		cv2.setWindowProperty("RGB image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-		cv2.rectangle(frame,(200,250),(300,350),(0,255,0),1)
+		
+		frame = get_video()
+		frame = cv2.flip(frame,1)
+		
+		#cv2.rectangle(frame,(200,250),(300,350),(0,255,0),1)
         #cv2.imshow('RGB image',frame)
 
         #get a frame from depth sensor
@@ -52,9 +59,12 @@ if __name__ == "__main__":
 				cY = int(M["m01"] / M["m00"])-8
 				#cv2.circle(frame, (cX, cY), 50, (255, 255, 255), 1)
 				cv2.rectangle(frame,(cX-50,cY-50),(cX+50,cY+50),(255,255,0),1)
-
-
-		cv2.imshow('RGB image',frame)
+		
+		#insert video to bg
+		h, w, c = frame.shape
+		bg[H/2-h/2:(H/2-h/2)+h, W/2-w/2:(W/2-w/2)+w] = frame
+		cv2.imshow('RGB image',bg)
+		
         # quit program when 'esc' key is pressed
 		k = cv2.waitKey(5) & 0xFF
 		if k == 27:
--- kinect/state.py
+++ kinect/state.py
@@ -5,6 +5,14 @@ import numpy as np
 threshold = 100
 current_depth = 650
 adj = 8
+state = 0
+bg = np.zeros([1080, 1920, 3], np.uint8)
+W = bg.shape[1]
+H = bg.shape[0]
+min_contours = 5000
+max_contours = 10000
+count = 0
+cap = cv2.VideoCapture("video0")
 
 #function to get RGB image from kinect
 def get_video():
@@ -20,17 +28,14 @@ def get_depth():
 
 if __name__ == "__main__":
     while 1:
-        #get a frame from RGB camera
-        # Setup SimpleBlobDetector parameters.
-		frame = get_video()
-		frame=cv2.flip(frame,1)
-
+		ret, cam = cap.read()
+		#set full screen mode
 		cv2.namedWindow("RGB image", cv2.WND_PROP_FULLSCREEN)
 		cv2.setWindowProperty("RGB image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-		cv2.rectangle(frame,(200,250),(300,350),(0,255,0),1)
-        #cv2.imshow('RGB image',frame)
-
+		
+		frame = get_video()
+		frame = cv2.flip(frame,1)
+		
         #get a frame from depth sensor
 		depth = get_depth()
 		depth=cv2.flip(depth,1)
@@ -43,18 +48,29 @@ if __name__ == "__main__":
 		#find contours form thresh
 		(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
-		#cnts = cnts[1]
 		for c in cnts:
-			# compute the center of the contour
-			M = cv2.moments(c)
-			if M["m00"] > 0:
-				cX = int(M["m10"] / M["m00"])-8
-				cY = int(M["m01"] / M["m00"])-8
-				#cv2.circle(frame, (cX, cY), 50, (255, 255, 255), 1)
-				cv2.rectangle(frame,(cX-50,cY-50),(cX+50,cY+50),(255,255,0),1)
-
-
-		cv2.imshow('RGB image',frame)
+			area = cv2.contourArea(c)
+			if area > min_contours and area < max_contours:
+				# compute the center of the contour
+				M = cv2.moments(c)
+				if M["m00"] > 0:
+					cX = int(M["m10"] / M["m00"])-8
+					cY = int(M["m01"] / M["m00"])-8
+					#cv2.circle(frame, (cX, cY), 50, (255, 255, 255), 1)
+					cv2.rectangle(frame,(cX-50,cY-50),(cX+50,cY+50),(255,255,0),1)
+					if count < 50:
+						count+=1
+					else:
+						count = 0
+						cv2.imwrite("test_cap.jpg", cam)
+				else:
+					count = 0
+		
+		#insert video to bg
+		h, w, c = frame.shape
+		bg[H/2-h/2:(H/2-h/2)+h, W/2-w/2:(W/2-w/2)+w] = frame
+		cv2.imshow('RGB image',bg)
+		
         # quit program when 'esc' key is pressed
 		k = cv2.waitKey(5) & 0xFF
 		if k == 27:
